5ec1a09a8ae7d9dd634b18fdc4d54c5f43f2c81b
Roman Medvedev <r.medvedev@nltu.lviv.ua>
2023-10-30 11:13:23 +0100

    fixed a few bags

--- lab3/ViewController.swift
+++ lab3/ViewController.swift
@@ -9,8 +9,6 @@ class ViewController: UIViewController {
     var initialColor: UIColor = UIColor.gray
     var selectedIndex: Int?
     var count: Int = 0
-    var winCards: [Bool] = [false,false,false,false,false,false]
-    
     var isFlipping = false
     
     override func viewDidLoad() {
@@ -22,35 +20,25 @@ class ViewController: UIViewController {
               let index = buttons.firstIndex(of: button) else {
             return
         }
-        
         let topCards = viewTopCards[index]
         let bottomCards = viewBottomCards[index]
         
-        if isFlipping || winCards[index] || topCards.isHidden {
+        if isFlipping  || topCards.isHidden {
             return
         }
         if !topCards.isHidden {
             UIView.transition(from: bottomCards, to: topCards, duration: 0.5, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
-        } else {
-            UIView.transition(from: topCards, to: bottomCards, duration: 0.5, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
         }
-        
         topCards.isHidden.toggle()
         bottomCards.isHidden.toggle()
         
-      
         if count == 0 {
             initialColor = bottomCards.backgroundColor!
             count += 1
             selectedIndex = index
-        } else {
-            if selectedIndex == index {
-                count = 0
-                return
-            } else {
+        }else
+        {
                 if initialColor == bottomCards.backgroundColor {
-                    winCards[selectedIndex!] = true
-                    winCards[index] = true
                     count = 0
                 } else {
                     isFlipping = true
@@ -65,9 +53,10 @@ class ViewController: UIViewController {
                         })
                         self.count = 0
                     }
+
                 }
             }
         }
     }
-}
+
 
